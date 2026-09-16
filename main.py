8ca7d0fcad06ccae0dd776190bee6f6086b4b605
ig-table-tennis <ivanrasa@live.com>
2026-09-17 01:07:41 +0200

    Implement error handling in main.py
    Added error handling to display exceptions in a scrollable view.

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
+import traceback
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
+
+try:
+
 from igtta import TenisMesaApp
 
 TenisMesaApp().run()
+
+except Exception:
+
+error = traceback.format_exc()
+
+class ErrorApp(App):
+
+    def build(self):
+
+        scroll = ScrollView()
+
+        label = Label(
+            text=error,
+            font_size="12sp",
+            size_hint_y=None,
+            halign="left",
+            valign="top"
+        )
+
+        label.bind(
+            texture_size=label.setter("size")
+        )
+
+        scroll.add_widget(label)
+
+        return scroll
+
+ErrorApp().run()
