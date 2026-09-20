fabf4577fb41cd91d04fefa0279b6cb59ca09fde
ig-table-tennis <ivanrasa@live.com>
2026-09-20 19:25:19 +0200

    Update entrada.py

--- widgets/entrada.py
+++ widgets/entrada.py
@@ -30,26 +30,13 @@ class Entrada(TextInput):
     @staticmethod
     def crear_titulo():
 
-        titulo = Label(
+        return Label(
             text="IG Table Tennis Analyzer",
-            font_name="fonts/Hand-Bold.ttf",
+            font_name="fonts/Hand-Bold.ttf", 
             size_hint=(1, None),
             height=dp(45),
-            font_size="22sp",
-            halign="center",
-            valign="bottom",
-            padding_y=dp(0)
+            font_size="22sp"
         )
-
-        titulo.bind(
-            size=lambda w, s: setattr(
-                w,
-                "text_size",
-                w.size
-            )
-        )
-
-        return titulo
 
     # ==================================
 
@@ -58,46 +45,9 @@ class Entrada(TextInput):
 
         fila = BoxLayout(
             size_hint=(1, None),
-            height=dp(60),
+            height=dp(50),
             spacing=dp(8)
         )
-
-        # ==================================
-        # FONDO DE LA FILA INFERIOR
-        # ==================================
-
-        with fila.canvas.before:
-
-            from kivy.graphics import Color, Rectangle
-
-            Color(
-                0.15,
-                0.15,
-                0.15,
-                1
-            )
-
-            fondo = Rectangle(
-                pos=fila.pos,
-                size=fila.size
-            )
-
-            fila.bind(
-                pos=lambda w, v: setattr(
-                    fondo,
-                    "pos",
-                    w.pos
-                ),
-                size=lambda w, v: setattr(
-                    fondo,
-                    "size",
-                    w.size
-                )
-            )
-
-        # ==================================
-        # BOTONES
-        # ==================================
 
         boton_nueva = IGButton(
             text="Nueva partida"
@@ -107,13 +57,8 @@ class Entrada(TextInput):
             text="Borrar Pantalla"
         )
 
-        boton_nueva.set_color(
-            (0.25, 0.25, 0.25, 1)
-        )
-
-        boton_borrar.set_color(
-            (0.25, 0.25, 0.25, 1)
-        )
+        boton_nueva.set_color((0.25, 0.25, 0.25, 1))
+        boton_borrar.set_color((0.25, 0.25, 0.25, 1))
 
         boton_nueva.bind(
             on_press=app.nueva_partida
@@ -123,12 +68,7 @@ class Entrada(TextInput):
             on_press=app.borrar
         )
 
-        fila.add_widget(
-            boton_nueva
-        )
-
-        fila.add_widget(
-            boton_borrar
-        )
+        fila.add_widget(boton_nueva)
+        fila.add_widget(boton_borrar)
 
         return fila
