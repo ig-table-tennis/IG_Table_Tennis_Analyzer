1afd07546f53bbac8557126f9f87c29a4b22beef
ig-table-tennis <ivanrasa@live.com>
2026-09-22 01:34:26 +0200

    Import Color and Rectangle from kivy.graphics

--- igtta.py
+++ igtta.py
@@ -8,6 +8,7 @@ from kivy.app import App
 from kivy.metrics import dp
 from kivy.clock import Clock
 from kivy.core.window import Window
+from kivy.graphics import Color, Rectangle
 
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
