b982eb008460b7190a55d3052c303b1197108bff
ig-table-tennis <ivanrasa@live.com>
2026-09-22 10:55:22 +0200

    Add background color and rectangle to canvas

--- igtta.py
+++ igtta.py
@@ -42,6 +42,23 @@ class TenisMesaApp(App):
             padding=dp(10),
             spacing=dp(10)
         )
+
+        with principal.canvas.before:
+            Color(0.3, 0.3, 0.3, 1)
+            principal._bg = Rectangle()
+
+            principal.bind(
+                pos=lambda w, v: setattr(
+                    principal._bg,
+                    "pos",
+                    w.pos
+                ),
+                size=lambda w, v: setattr(
+                    principal._bg,
+                    "size",
+                    w.size
+                )
+            ) 
         
         # ==========================================
         # VARIABLES
